b26b0b2f197ed2ea8fe87fe37c398be6c936f3d5
masaki39 <masa21gifus@gmail.com>
2026-02-22 22:35:17 +0900

    yazi:delete bunny

--- yazi/init.lua
+++ yazi/init.lua
@@ -6,18 +6,3 @@ th.git.ignored_sign = "!"
 th.git.deleted_sign = "D"
 th.git.updated_sign = "S"
 require("git"):setup()
-require("bunny"):setup({
-	hops = {
-		{ key = "h", path = "~", desc = "Home directory" },
-		{ key = ".", path = "~/ghq/github.com/masaki39/dotfiles", desc = "Dotfiles" },
-		{ key = "d", path = "~/Downloads", desc = "Downloads" },
-		{ key = "o", path = "~/Documents/masaki39-core", desc = "Obsidian" },
-		{ key = "p", path = "~/pCloud Drive", desc = "pCloud Drive" },
-		{ key = "q", path = "~/ghq/github.com/masaki39", desc = "GitHub" },
-	},
-	desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
-	ephemeral = true, -- Enable ephemeral hops, default is true
-	tabs = true, -- Enable tab hops, default is true
-	notify = false, -- Notify after hopping, default is false
-	fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
-})
