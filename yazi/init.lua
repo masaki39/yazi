0f2d2a3e5235b0a3984bc303adee935552a9dba4
masaki39 <masa21gifus@gmail.com>
2026-02-27 15:34:14 +0900

    yazi:update git plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,8 +1,12 @@
+require("starship"):setup()
 th.git = th.git or {}
 th.git.modified_sign = "M"
+th.git.modified = ui.Style():fg("red")
 th.git.added_sign = "A"
-th.git.untracked_sign = "?"
+th.git.untracked_sign = "??"
+th.git.untracked = ui.Style():fg("red")
 th.git.ignored_sign = "!"
+th.git.ignored = ui.Style():fg("blue")
 th.git.deleted_sign = "D"
 th.git.updated_sign = "S"
 require("git"):setup()
