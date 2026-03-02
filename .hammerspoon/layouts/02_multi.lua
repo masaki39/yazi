791058e07546fbe4e5e347489ad2c93f40b69695
masaki39 <masa21gifus@gmail.com>
2026-03-02 22:42:42 +0900

    hammerspoon:update layout

--- .hammerspoon/layouts/02_multi.lua
+++
@@ -1,9 +0,0 @@
-return {
-	keybind = "ctrl+alt+2",
-	description = "Multi: multi task ",
-	windows = {
-		{ app = "Obsidian", screen = 1, x = 0, y = 0, w = 0.5, h = 1 },
-		{ app = "Google Chrome", screen = 1, x = 0.5, y = 0, w = 0.5, h = 1 },
-		{ app = "Ghostty", screen = 0, x = 0, y = 0, w = 1, h = 1, focus = true },
-	},
-}
---
+++ .hammerspoon/layouts/02_o-c-g.lua
@@ -0,0 +1,9 @@
+return {
+	keybind = "ctrl+alt+2",
+	description = "Obsidian --> Google Chrome --> Ghostty",
+	windows = {
+		{ app = "Obsidian", screen = 1, x = 0, y = 0, w = 0.5, h = 1, focus = true },
+		{ app = "Google Chrome", screen = 1, x = 0.5, y = 0, w = 0.5, h = 1 },
+		{ app = "Ghostty", screen = 0, x = 0, y = 0, w = 1, h = 1 },
+	},
+}
