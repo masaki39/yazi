0bf2b556bf69bd25e567a19040c93e9a9480dda9
masaki39 <masa21gifus@gmail.com>
2026-03-04 08:46:49 +0900

    spoon:update jinrai

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -26,83 +26,103 @@ spoon.SpoonInstall:andUse("Ryoiki", {
 	hotkeys = { showChooser = { { "ctrl", "alt" }, "m" } },
 })
 
-local jinrai = dofile("/Users/masaki/ghq/github.com/tadashi-aikawa/jinrai/init.lua")
+-- Jinrai
+spoon.SpoonInstall.repos.jinrai = {
+	url = "https://github.com/tadashi-aikawa/jinrai",
+	desc = "JINRAI Spoon repository",
+	branch = "spoons",
+}
 
-jinrai.setup({
-	focus_border = {
-		borderWidth = 10,
-		borderColor = { red = 0.40, green = 0.68, blue = 0.98, alpha = 0.95 },
-		outlineWidth = 2,
-		outlineColor = { red = 0, green = 0, blue = 0, alpha = 0.70 },
-		duration = 0.5,
-		fadeSteps = 18,
-		cornerRadius = 10,
-		minWindowSize = 480,
-	},
-	window_hints = {
-		hintChars = {
-			"A",
-			"S",
-			"D",
-			"F",
-			"G",
-			"H",
-			"J",
-			"K",
-			"L",
-			"Q",
-			"W",
-			"E",
-			"R",
-			"T",
-			"Y",
-			"U",
-			"I",
-			"O",
-			"P",
-			"Z",
-			"X",
-			"C",
-			"V",
-			"B",
-			"N",
-			"M",
-		},
-		appPrefixOverrides = {
-			{
-				match = { bundleID = "com.google.Chrome" },
-				prefix = "C",
+spoon.SpoonInstall:andUse("Jinrai", {
+	repo = "jinrai",
+	fn = function(jinrai)
+		jinrai:setup({
+			focus_border = {
+				visual = {
+					border = {
+						width = 10,
+						color = { red = 0.40, green = 0.68, blue = 0.98, alpha = 0.95 },
+					},
+					outline = {
+						width = 2,
+						color = { red = 0, green = 0, blue = 0, alpha = 0.70 },
+					},
+					cornerRadius = 10,
+				},
+				animation = {
+					duration = 0.5,
+					fadeSteps = 18,
+				},
+				window = {
+					minSize = 480,
+				},
 			},
-		},
-		focusBackKey = "tab",
-		directionKeys = {
-			left = "h",
-			down = "j",
-			up = "k",
-			right = "l",
-			upLeft = "y",
-			upRight = "u",
-			downLeft = "b",
-			downRight = "n",
-		},
-		hotkeyModifiers = { "alt", "shift", "cmd", "ctrl" },
-		hotkeyKey = "j",
-		swapWindowFrameSelectModifiers = { "shift" },
-		iconSize = 72,
-		titleMaxSize = 72,
-		centerCursor = true,
-		onError = function(err)
-			hs.alert.show("Window Hints error: " .. tostring(err), 3)
-		end,
-	},
-	focus_back = {
-		hotkeyModifiers = { "option", "shift", "cmd", "ctrl" },
-		hotkeyKey = "tab",
-		centerCursor = true,
-		stateSync = {
-			interval = 0.15,
-			targetApps = { "com.mitchellh.ghostty" },
-			historyScope = "application",
-		},
-	},
+			window_hints = {
+				hotkey = {
+					modifiers = { "alt", "shift", "cmd", "ctrl" },
+					key = "j",
+				},
+				hint = {
+					chars = {
+						"A", "S", "D", "F", "G", "H", "J", "K", "L",
+						"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+						"Z", "X", "C", "V", "B", "N", "M",
+					},
+					prefixOverrides = {
+						{
+							match = { bundleID = "com.google.Chrome" },
+							prefix = "C",
+						},
+					},
+				},
+				navigation = {
+					focusBackKey = "tab",
+					directionKeys = {
+						left = "h",
+						down = "j",
+						up = "k",
+						right = "l",
+						upLeft = "y",
+						upRight = "u",
+						downLeft = "b",
+						downRight = "n",
+					},
+					directHotkeys = {
+						modifiers = { "ctrl", "alt", "shift", "cmd" },
+						keys = {
+							left = "left",
+							down = "down",
+							up = "up",
+							right = "right",
+						},
+					},
+					swapSelectModifiers = { "shift" },
+				},
+				ui = {
+					icon = { size = 72 },
+					text = { titleMaxSize = 72 },
+				},
+				behavior = {
+					centerCursor = true,
+					onError = function(err)
+						hs.alert.show("Window Hints error: " .. tostring(err), 3)
+					end,
+				},
+			},
+			focus_back = {
+				hotkey = {
+					modifiers = { "option", "shift", "cmd", "ctrl" },
+					key = "tab",
+				},
+				behavior = {
+					centerCursor = true,
+				},
+				stateSync = {
+					interval = 0.15,
+					targetApps = { "com.mitchellh.ghostty" },
+					historyScope = "application",
+				},
+			},
+		})
+	end,
 })
