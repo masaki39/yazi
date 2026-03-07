0ffc56922914eb51cc9c785b9c13b4f96e032b3c
masaki39 <masa21gifus@gmail.com>
2026-03-07 21:09:26 +0900

    spoon:update layout

--- .hammerspoon/layouts/07_browse.lua
+++ .hammerspoon/layouts/07_browse.lua
@@ -1,8 +1,8 @@
 return {
 	keybind = "ctrl+alt+7",
-	description = "Browse: Chrome | Ghostty",
+	description = "Chrome × 2",
 	windows = {
-		{ app = "Google Chrome", screen = 1, x = 0, y = 0, w = 0.6, h = 1, focus = true },
-		{ app = "Ghostty", screen = 1, x = 0.6, y = 0, w = 0.4, h = 1 },
+		{ app = "Google Chrome", screen = 1, x = 0, y = 0, w = 0.5, h = 1, focus = true },
+		{ app = "Google Chrome", screen = 1, x = 0.5, y = 0, w = 0.5, h = 1 },
 	},
 }
